cba0f501959e70e17475547f72aa91ebd8d23cb9
brian <>
2025-12-16 01:35:23 +0000

    Day7 part2
    FossilOrigin-Name: f99c8e8991cf4d805a909a2121a418b9ce63dae3f2c29031fea6e2c3a76ad47f

--- day7/day7.go
+++ day7/day7.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"github.com/BooleanCat/go-functional/v2/it"
 	"github.com/baritonehands/aoc-2025-go/utils"
+	"maps"
 	"regexp"
 	"slices"
 	"strings"
 )
 
 //go:embed input.txt
-var input string
+var input string //= ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n..............."
 
 func main() {
 	lines := strings.Split(input, "\n")
@@ -45,8 +46,52 @@ func main() {
 			}
 
 			beamSet = nextBeamSet
-			fmt.Println(nextBeamSet.Values())
 		}
 	}
 	fmt.Println("part1", part1)
+
+	part2 := map[int]int64{start: 1}
+	toAdd := map[int]int64{}
+	toDelete := utils.Set[int]{}
+	fmt.Println(part2)
+	for _, line := range lines[1:] {
+		splits := splitRegex.FindAllStringIndex(line, -1)
+		if len(splits) > 0 {
+			splitSet := utils.SeqSet(it.Map(slices.Values(splits), func(split []int) int {
+				return split[0]
+			}))
+
+			clear(toAdd)
+			clear(toDelete)
+			for last, cnt := range part2 {
+				if splitSet.Contains(last) {
+					if last > 0 {
+						if _, ok := toAdd[last-1]; !ok {
+							toAdd[last-1] = 0
+						}
+						toAdd[last-1] += cnt
+					}
+					if last <= xMax {
+						if _, ok := toAdd[last+1]; !ok {
+							toAdd[last+1] = 0
+						}
+						toAdd[last+1] += cnt
+					}
+					toDelete[last] = true
+				}
+			}
+			for last, cnt := range toAdd {
+				part2[last] += cnt
+			}
+			for last := range toDelete {
+				delete(part2, last)
+			}
+			//fmt.Println(part2)
+		}
+	}
+	part2Sum := int64(0)
+	for v := range maps.Values(part2) {
+		part2Sum += v
+	}
+	fmt.Println("part2", part2Sum)
 }
